ca42b113928afab088b0ae61130db0e72f05a94d
Anjali <anjali.trace13@gmail.com>
2023-03-04 15:04:29 +0000

    Prettify header

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Image, View, Text } from "react-native";
 import { FinalChoices } from "./src/pages/finalChoices";
 
 const App = () => {
   return (
     <View>
-      <View style={styles.stuckContainer}>
-        <Text>Who's that Pokémon?</Text>
+      <View style={styles.header}>
+        <View style={styles.headerContent}>
+          <Text style={styles.headerText}>Who's that </Text>
+          <Image
+            source={require("./assets/logo.png")}
+            style={{ height: 50, width: 54 }}
+          />
+          <Text style={styles.headerText}> Pokémon</Text>
+        </View>
       </View>
       <ScrollView style={styles.scrollingContainer}>
         <FinalChoices />
@@ -15,12 +22,20 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  stuckContainer: {
-    height: "20%",
-    justifyContent: "flex-end",
+  header: {
+    height: "30%",
+    justifyContent: "center",
     alignItems: "center",
+    // verticalAlign: "bottom",
     backgroundColor: "red",
+    paddingTop: "10%",
+    paddingBottom: 7,
   },
+  headerContent: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  headerText: { color: "white" },
   scrollingContainer: {
     backgroundColor: "green",
   },
