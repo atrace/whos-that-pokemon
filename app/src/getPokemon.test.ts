8fd65ef022dcdc3334a2ff1dc4aea37f8d7ced30
Anjali Trace <anjali.trace@infinityworks.com>
2023-12-18 16:13:00 +0000

    Update tests to reflect current behaviour

--- app/src/getPokemon.test.ts
+++ app/src/getPokemon.test.ts
@@ -1,65 +1,84 @@
-import {
-  getPokemonByName,
-  getPokemonNames,
-  getPrettyPokemonNames
-} from "./getPokemon";
+import { getPokeClient, getPrettyPokemonNames } from "./getPokemon";
 import { ditto as examplePokemon } from "./pokemon";
 
-describe("getPokemonByName", () => {
-  it("should get a Pokemon", async () => {
-    // Arrange
-    const pokemonName = "ditto";
+describe("getPokemon", () => {
+  let client;
 
-    // Act
-    const pokemon = await getPokemonByName(pokemonName);
-
-    // Assert
-    expect(pokemon).toBeDefined();
-    expect(pokemon).toEqual(examplePokemon);
-  });
-});
-
-describe("getPokemonNames", () => {
-  it("should get first 20 pokemon if no offset provided", async () => {
-    // Arrange / Act
-    const pokemonList = await getPokemonNames();
-
-    // Assert
-    expect(pokemonList).toBeDefined();
-    expect(pokemonList.count).toEqual(1279);
-    expect(pokemonList.next).toEqual(
-      "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20"
-    );
-    expect(pokemonList.previous).toBeNull();
-    expect(pokemonList.results.length).toEqual(20);
+  beforeAll(() => {
+    client = getPokeClient();
   });
 
-  it("should get second 20 pokemon if offset of 20 provided", async () => {
-    // Arrange / Act
-    const pokemonList = await getPokemonNames(20);
+  describe("getPokemonByName", () => {
+    it("should get a Pokemon", async () => {
+      // Arrange
+      const pokemonName = "ditto";
 
-    // Assert
-    expect(pokemonList).toBeDefined();
-    expect(pokemonList.count).toEqual(1279);
-    expect(pokemonList.next).toEqual(
-      "https://pokeapi.co/api/v2/pokemon?offset=40&limit=20"
-    );
-    expect(pokemonList.previous).toEqual(
-      "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
-    );
-    expect(pokemonList.results.length).toEqual(20);
+      // Act
+      const pokemon = await client.getPokemonByName(pokemonName);
+
+      // Assert
+      expect(pokemon).toBeDefined();
+      expect(pokemon).toEqual(examplePokemon);
+    });
   });
-});
 
-describe("getPrettyPokemonNames", () => {
-  it("should get first 20 pokemon if no offset provided", async () => {
-    // Arrange / Act
-    const prettyNames = await getPrettyPokemonNames();
+  describe("getPokemonNames", () => {
+    it("should get first 20 pokemon if no offset provided", async () => {
+      // Arrange / Act
+      const pokemonList = await client.getPokemonNames();
+      console.log("pokemonList:", pokemonList);
 
-    // Assert
-    expect(prettyNames).toBeDefined();
-    expect(prettyNames.length).toEqual(20);
-    expect(prettyNames[0].label).toEqual("Bulbasaur");
-    expect(prettyNames[0].value).toEqual("bulbasaur");
+      // Assert
+      expect(pokemonList).toBeDefined();
+      expect(pokemonList.length).toEqual(20);
+      expect(pokemonList[0].value).toEqual("bulbasaur"); // pokemon #1
+    });
+
+    it("should get second 20 pokemon if offset of 20 provided", async () => {
+      // Arrange / Act
+      const pokemonList = await client.getPokemonNames(20);
+
+      // Assert
+      expect(pokemonList).toBeDefined();
+      expect(pokemonList.length).toEqual(20);
+      expect(pokemonList[0].value).toEqual("spearow"); // pokemon #20
+    });
+  });
+
+  describe("getPrettyPokemonNames", () => {
+    it("should get first 20 pokemon if no offset provided", () => {
+      // Arrange
+      const pokemonNamesToPrettify = [
+        { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
+        { name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/" },
+        { name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/" },
+        { name: "charmander", url: "https://pokeapi.co/api/v2/pokemon/4/" },
+        { name: "charmeleon", url: "https://pokeapi.co/api/v2/pokemon/5/" },
+        { name: "charizard", url: "https://pokeapi.co/api/v2/pokemon/6/" },
+        { name: "squirtle", url: "https://pokeapi.co/api/v2/pokemon/7/" },
+        { name: "wartortle", url: "https://pokeapi.co/api/v2/pokemon/8/" },
+        { name: "blastoise", url: "https://pokeapi.co/api/v2/pokemon/9/" },
+        { name: "caterpie", url: "https://pokeapi.co/api/v2/pokemon/10/" },
+        { name: "metapod", url: "https://pokeapi.co/api/v2/pokemon/11/" },
+        { name: "butterfree", url: "https://pokeapi.co/api/v2/pokemon/12/" },
+        { name: "weedle", url: "https://pokeapi.co/api/v2/pokemon/13/" },
+        { name: "kakuna", url: "https://pokeapi.co/api/v2/pokemon/14/" },
+        { name: "beedrill", url: "https://pokeapi.co/api/v2/pokemon/15/" },
+        { name: "pidgey", url: "https://pokeapi.co/api/v2/pokemon/16/" },
+        { name: "pidgeotto", url: "https://pokeapi.co/api/v2/pokemon/17/" },
+        { name: "pidgeot", url: "https://pokeapi.co/api/v2/pokemon/18/" },
+        { name: "rattata", url: "https://pokeapi.co/api/v2/pokemon/19/" },
+        { name: "raticate", url: "https://pokeapi.co/api/v2/pokemon/20/" }
+      ];
+
+      // Act
+      const prettyNames = getPrettyPokemonNames(pokemonNamesToPrettify);
+
+      // Assert
+      expect(prettyNames).toBeDefined();
+      expect(prettyNames.length).toEqual(20);
+      expect(prettyNames[0].label).toEqual("Bulbasaur");
+      expect(prettyNames[0].value).toEqual("bulbasaur");
+    });
   });
 });
